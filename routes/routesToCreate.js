9b54f6229adb1f023d40888a1eff19498ae68ef9
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-10-14 11:00:33 -0400

    update

--- routes/routesToCreate.js
+++ routes/routesToCreate.js
@@ -10,26 +10,52 @@ const games = require('../models/games.js')
 
 
 // get all
-router.get('/all-games',(req,res)=>{
-  if(games.name !== games.params.name){
-return res.status(400).json(games)({confirmation:'fail',message:'no games found'})
-    }else{
-        return res.status(200).json({ confirmation: 'success', games }
-        )
-}})
+router.get('/all-games', (req, res) => {
+    if (games.name !== games.params.name) {
+        return res.status(400).json(games)({
+            confirmation: 'fail',
+            message: 'no games found'
+        })
+    } else {
+        return res.status(200).json({
+            confirmation: 'success',
+            games
+        })
+    }
+})
 
 
+//getSingleGame
+router.get('/single-game/:id', (req, res) => {
+    let foundGame = games.filter((game) => {
+        if (game.id === req.params.id) {
+            return res.status(200).json({
+                confirmation: 'success',
+                games
+            });
+        }
+    });
+    if (!foundGame.length)
+        return res
+            .status(400)
+            .json({
+                confirmation: 'fail',
+                message: 'Game Does Not Exist'
+            });
+});
 
 
 //delete
-router.delete('/delete-game:id',(req, res) => {
+router.delete('/delete-game:id', (req, res) => {
     //filter game based on id parameter in address
     let removeGame = games.filter((foundGame) => {
-      return foundGameno.id !== req.params.id;
+        return foundGameno.id !== req.params.id;
     });
     //mutate games array and replace with removeUser array
     games = removeGame;
     //return results
-    return res.status(200).json({ confirmation: 'success', games }
-    )})
-
+    return res.status(200).json({
+        confirmation: 'success',
+        games
+    })
+})

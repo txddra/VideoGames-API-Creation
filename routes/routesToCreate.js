60e5c1efecf7318b60c68994073d53b4d807fe20
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-10-14 11:31:34 -0400

    added create game

--- routes/routesToCreate.js
+++ routes/routesToCreate.js
@@ -44,6 +44,36 @@ router.get('/single-game/:id', (req, res) => {
             });
 });
 
+//createGame
+router.post('/create-game', (req, res) => {
+    //check if game exists
+    let existingGame = games.filter(
+      (foundGame) => foundGame.name === req.body.description
+    );
+    if (existingGame.length) {
+      return res.status(400).send('Game Already Exists');
+    }
+  
+    //create a new user object
+    const newGame = {};
+  
+    //values for newUser based on req.body inputs in postman
+    newUser.name = req.body.name;
+    newUser.description = req.body.description;
+    newUser.id = String(games.length + 1);
+    // add user to array
+    games.push(newGame);
+    //return the new user
+    return res.status(200).json({ confirmation: 'success', newGame });
+  });
+  
+
+
+
+
+
+
+
 
 //delete
 router.delete('/delete-game:id', (req, res) => {
